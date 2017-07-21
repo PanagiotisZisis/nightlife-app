cace5b9071c31869ca80ab75ead7dbd5c189a2b0
Panagiotis Zisis <takis.zisis@gmail.com>
2017-07-21 18:13:31 +0300

    removed comments

--- routes/index.js
+++ routes/index.js
@@ -30,39 +30,14 @@ router.get('/', (req, res) => {
         res.render('index', { location: location, bars: response.jsonBody.businesses, user: false, error: false });
       }).catch(() => {
         if (req.user) {
-          return res.render('index', { location: false, bars: false , user: req.user, error: 'Invalid Search Term' });
+          return res.render('index', { location: false, bars: false , user: req.user, error: 'There was an error - please try again.' });
         }
         res.cookie('location', location);
-        res.render('index', { location: false, bars: false , user: false, error: 'Invalid Search Term' });
+        res.render('index', { location: false, bars: false , user: false, error: 'There was an error - please try again.' });
       });
     });
   }
 });
-
-/*router.post('/', (req, res) => {
-  const location = req.body.location;
-  req.user.location = location;
-  
-  yelp.accessToken(process.env.YELP_CLIENT_ID, process.env.YELP_CLIENT_SECRET).then(response => {
-    const client = yelp.client(response.jsonBody.access_token);
-
-    client.search({
-      term: 'Nightlife',
-      categories: 'bars',
-      location: location
-    }).then(response => {
-      if (req.user) {
-        return res.render('index', { location: location, bars: response.jsonBody.businesses, user: req.user, error: false });
-      }
-      res.render('index', { location: location, bars: response.jsonBody.businesses, user: false, error: false });
-    }).catch(() => {
-      if (req.user) {
-        return res.render('index', { location: false, bars: false , user: req.user, error: 'Invalid Search Term' });
-      }
-      res.render('index', { location: false, bars: false , user: false, error: 'Invalid Search Term' });
-    });
-  });
-});*/
 
 router.get('/auth/facebook', passport.authenticate('facebook'));
 
